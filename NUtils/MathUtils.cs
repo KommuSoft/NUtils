cbb89b4e2e5c8e74353c11ff805085ae61937c54
KommuSoft <vanonsem.willem@gmail.com>
2014-05-27 20:53:01 +0200

    random gen

---
+++ NUtils/MathUtils.cs
@@ -0,0 +1,48 @@
+//
+//  MathUtils.cs
+//
+//  Author:
+//       Willem Van Onsem <vanonsem.willem@gmail.com>
+//
+//  Copyright (c) 2014 Willem Van Onsem
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace NUtils {
+	/// <summary>
+	/// A utility class that contains mathematical constants, functions and random number generators.
+	/// </summary>
+	public static class MathUtils {
+
+		private static Random random = new Random ();
+
+		public static int Next () {
+			return random.Next ();
+		}
+
+		public static int Next (int maxValue) {
+			return random.Next (maxValue);
+		}
+
+		public static int Next (int minValue, int maxValue) {
+			return random.Next (minValue, maxValue);
+		}
+
+		public static double NextDouble () {
+			return random.NextDouble ();
+		}
+	}
+}
+
