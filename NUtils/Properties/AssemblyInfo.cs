d8212cdd8a7a98a6bd9f7a8b561723d6301a5f0a
KommuSoft <vanonsem.willem@gmail.com>
2014-05-27 21:26:14 +0200

    assembly info

--- NUtils/Properties/AssemblyInfo.cs
+++ NUtils/Properties/AssemblyInfo.cs
@@ -4,17 +4,17 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle ("NUtils")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("A set of utility classes and functions used in most .NET projects.")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("KU Leuven")]
+[assembly: AssemblyCompany ("KU Leuven, Department of Computer Science, Celestijnenlaan 200A, 3001 Heverlee, Belgium")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Willem Van Onsem")]
+[assembly: AssemblyCopyright ("2014, Willem Van Onsem")]
 [assembly: AssemblyTrademark ("DTAI")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.1.*")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
