f8477953b93f2dc65fbac8723851a27d39d32bcf
KommuSoft <vanonsem.willem@gmail.com>
2014-11-15 18:42:00 +1100

    constructors for type validators

--- NUtils/QueryPath/PathNode.cs
+++ NUtils/QueryPath/PathNode.cs
@@ -19,7 +19,10 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.Linq;
 using NUtils.Designpatterns;
+using System.Collections.Generic;
+using NUtils.Abstract;
 
 namespace NUtils.QueryPath {
 
@@ -30,14 +33,50 @@ namespace NUtils.QueryPath {
 	/// <typeparam name='TType'>The type of the nodes that will be accepted.</typeparam>
 	public class PathNode<T,TType> : PathNodeBase<T> where T : IComposition<T> {
 
+		#region Fields
+		/// <summary>
+		/// A <see cref="T:List`1"/> of <see cref="T:IValidator`1"/> instance that must all hold such that the node is accepted.
+		/// </summary>
+		private readonly List<IValidater<T>> validators = null;
+		#endregion
 		#region Constructors
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:PathNode`2"/> class.
+		/// Initializes a new instance of the <see cref="T:PathNode`2"/> class with no additional validators.
 		/// </summary>
 		/// <remarks>
-		/// <para>No parameters are required since the type is handled by the typeparameter.</para>
+		/// <para>Note that there is an implicit constraint to the <typeparamref name="TType"/> type parameter.</para>
 		/// </remarks>
 		public PathNode () {
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:PathNode`2"/> class with an optional list of validators.
+		/// </summary>
+		/// <param name='validators'>A list of validators that must be fulfilled, optional, if not effective, there are no additional validators.</param>
+		/// <remarks>
+		/// <para>Note that there is an implicit constraint to the <typeparamref name="TType"/> type parameter.</para>
+		/// <para>Non effective <paramref name="validators"/> are ignored.</para>
+		/// </remarks>
+		public PathNode (IEnumerable<IValidater<T>> validators) {
+			if (validators != null) {
+				List<IValidater<T>> lst = validators.Effectives ().ToList ();
+				if (lst.Count > 0x00) {
+					this.validators = lst;
+				}
+			} else {
+				this.validators = null;
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:PathNode`2"/> class with an optional list of validators.
+		/// </summary>
+		/// <param name='validators'>An array of validators that must be fulfilled.</param>
+		/// <remarks>
+		/// <para>Note that there is an implicit constraint to the <typeparamref name="TType"/> type parameter.</para>
+		/// <para>Non effective <paramref name="validators"/> are ignored.</para>
+		/// </remarks>
+		public PathNode (params IValidater<T>[] validators) : this((IEnumerable<IValidater<T>>) validators) {
 		}
 		#endregion
 		#region IValidater implementation
@@ -75,12 +114,29 @@ namespace NUtils.QueryPath {
 
 		#region Constructors
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:PathNode`1"/> class.
+		/// Initializes a new instance of the <see cref="T:PathNode`1"/> class with no additional validators.
 		/// </summary>
+		public PathNode () : base() {
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:PathNode`1"/> class with an optional list of validators.
+		/// </summary>
+		/// <param name='validators'>A list of validators that must be fulfilled, optional, if not effective, there are no additional validators.</param>
 		/// <remarks>
-		/// <para>No parameters are required since the type is handled by the typeparameter.</para>
+		/// <para>Non effective <paramref name="validators"/> are ignored.</para>
 		/// </remarks>
-		public PathNode () {
+		public PathNode (IEnumerable<IValidater<T>> validators) : base(validators) {
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:PathNode`1"/> class with an optional list of validators.
+		/// </summary>
+		/// <param name='validators'>An array of validators that must be fulfilled.</param>
+		/// <remarks>
+		/// <para>Non effective <paramref name="validators"/> are ignored.</para>
+		/// </remarks>
+		public PathNode (params IValidater<T>[] validators) : base(validators) {
 		}
 		#endregion
 	}
